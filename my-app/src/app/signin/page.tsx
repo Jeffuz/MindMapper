408e28d98c01e8453b6d8c69a401e33f7b594a3d
Simon Huang <huisimonliu@gmail.com>
2024-08-14 00:02:08 -0700

    Signup page responsive and form submit working

--- my-app/src/app/signin/page.tsx
+++ my-app/src/app/signin/page.tsx
@@ -2,16 +2,16 @@ const Signin = () => {
 
   return (
     <div className="flex h-screen items-center justify-center bg-teal1">
-      <div className="p-10 rounded-md block ml-[10%] mr-[10%] bg-slate-50 drop-shadow-md">
+      <div className="p-5 md:p-10 rounded-md block ml-[10%] mr-[10%] bg-slate-50 drop-shadow-md">
         <h1>Sign up</h1>
-        <form className="p-2">
-          <label for="email" className="block">E-Mail</label>
+        <form action="/api/signup" method="POST" className="p-2">
+          <label for="email" className="lg:text-lg text-sm block">E-Mail</label>
           <input type="text" name="email" className="block pl-1 border-[1px] border-orange1 rounded-sm"/>
           <br/>
-          <label for="password" className="block">Password</label>
+          <label for="password" className="lg:text-lg text-sm block">Password</label>
           <input type="password" name="password" className="block pl-1 rounded-sm border-[1px] border-orange1"/>
           <br/>
-          <button type="submit" className="w-full p-1 rounded-sm bg-orange1 hover:bg-orange3 active:bg-teal1 transition-colors duration-500">Sign Up</button>
+          <button type="submit" className="w-full lg:text-lg text-sm p-1 rounded-sm bg-orange1 hover:bg-orange3 active:bg-teal1 transition-colors duration-500">Sign Up</button>
         </form>
       </div>
 
