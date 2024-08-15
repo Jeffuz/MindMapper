8aa83972e0f5b624c9e71b895871a2904dba2626
Jeff Zhang <52511888+Jeffuz@users.noreply.github.com>
2024-08-15 23:38:57 +0800

    create template for creation

---
+++ my-app/src/app/components/dashboard/create_card.tsx
@@ -0,0 +1,9 @@
+
+
+const create_card = () => {
+  return (
+    <div>create_card</div>
+  )
+}
+
+export default create_card
--- my-app/src/app/dashboard/creation/page.tsx
+++ my-app/src/app/dashboard/creation/page.tsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from "react";
+import Navbar from "@/app/components/navbar";
 
 const creation = () => {
   return (
-    <div>creation</div>
-  )
-}
+    <div>
+      <Navbar />
+      {/* Bar (title + create)*/}
+      <div className="flex justify-between">
+        <div>Create a new flashcard set</div>
+        <button>Create</button>
+      </div>
+      {/* Title, under is description */}
+      <div></div>
+      {/* Modal to Generate cards using llm */}
+      <div></div>
+      {/* Cards */}
+      <div></div>
+      {/* Add Card */}
+      <div></div>
+      {/* Create Deck*/}
+      <div>
+        <button>Create</button>
+      </div>
+    </div>
+  );
+};
 
-export default creation
+export default creation;
