8b309baccd74c9f6ff9046eb4d3d3c4ad5b89a1b
Jeff Zhang <52511888+Jeffuz@users.noreply.github.com>
2024-08-15 15:06:13 +0800

    add google analytics

--- my-app/src/app/layout.tsx
+++ my-app/src/app/layout.tsx
@@ -1,12 +1,14 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "MindMapper",
-  description: "Unlock smarter learning with our AI-powered flashcard generator.",
+  description:
+    "Unlock smarter learning with our AI-powered flashcard generator.",
 };
 
 export default function RootLayout({
@@ -16,6 +18,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID}`}
+        ></Script>
+        <Script id="google-analytics">
+          {`
+            window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', '${process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID}');
+          `}
+        </Script>
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
