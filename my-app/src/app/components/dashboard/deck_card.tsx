b42bab9ba7597c75c73c546c3f415cba56307f2f
Simon Huang <huisimonliu@gmail.com>
2024-08-15 18:15:45 -0700

    Updated CSS for dashboard hover effects

--- my-app/src/app/components/dashboard/deck_card.tsx
+++ my-app/src/app/components/dashboard/deck_card.tsx
@@ -2,7 +2,8 @@ import { LuBookOpen } from "react-icons/lu";
 
 const deck_card = ({ title, subtitle, amount }: any) => {
   return (
-    <div className="flex flex-col justify-between bg-teal2 rounded-lg shadow-lg p-5 aspect-video hover:animate-pulse">
+    <div className="flex flex-col justify-between bg-teal2 rounded-lg 
+    shadow-lg p-5 aspect-video hover:brightness-125 transition-all duration-200">
       {/* Titles */}
       <div className="flex flex-col">
         {/* title */}
@@ -17,10 +18,11 @@ const deck_card = ({ title, subtitle, amount }: any) => {
       </div>
       {/* Button */}
       <div className="w-full flex justify-between md:flex-row flex-col gap-3">
-        <button className="w-full md:w-[70%] bg-orange1 hover:bg-orange1/90 py-2 text-white rounded-lg shadow-lg transition duration-500">
+        <button className="w-full md:w-[70%] bg-orange1 hover:bg-orange-600/80 py-2 text-white 
+        rounded-lg shadow-lg transition duration-200">
           Open Deck
         </button>
-        <button className="w-full md:w-[30%] hover:bg-white rounded-lg transition duration-500">
+        <button className="w-full md:w-[30%] hover:bg-white rounded-lg transition duration-200">
           Edit
         </button>
       </div>
